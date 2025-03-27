53435664dd9fd3f6c1380dd6c6b2a2513bdd9066
someaddons <someaddons@github.com>
2025-03-27 11:31:59 +0100

    1.4: Now also automatically stops walking when you're mining a block that doesnt break right away. Controlled by the "stopOnInteractWithBlock" config option

--- src/main/java/com/autowalk/mixin/MultiPlayerGameModeMixin.java
+++ src/main/java/com/autowalk/mixin/MultiPlayerGameModeMixin.java
@@ -4,13 +4,18 @@ import com.autowalk.AutoWalk;
 import com.autowalk.AutoWalkClient;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,13 +24,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin
 {
+    @Shadow private float destroyTicks;
+
     @Inject(method = "performUseItemOn", at = @At("RETURN"))
-    private void checkResult(final LocalPlayer p_233747_, final InteractionHand p_233748_, final BlockHitResult p_233749_, final CallbackInfoReturnable<InteractionResult> cir)
+    private void checkResult(final LocalPlayer player, final InteractionHand p_233748_, final BlockHitResult blockHitResult, final CallbackInfoReturnable<InteractionResult> cir)
     {
         if (AutoWalk.config.getCommonConfig().stopOnInteractWithBlock)
         {
             if (cir.getReturnValue().consumesAction())
             {
+                if (blockHitResult.getType() == HitResult.Type.BLOCK && (player.level().getBlockState(blockHitResult.getBlockPos()).is(BlockTags.DOORS) || player.level().getBlockState(blockHitResult.getBlockPos()).is(BlockTags.FENCE_GATES)))
+                {
+                    return;
+                }
                 AutoWalkClient.AUTO_RUN_ENABLED = false;
             }
         }
@@ -59,6 +70,24 @@ public class MultiPlayerGameModeMixin
         final Player p_105227_, final Entity entity, final InteractionHand p_105229_, final CallbackInfoReturnable<InteractionResult> cir)
     {
         if (AutoWalk.config.getCommonConfig().stopOnInteractWithEntity)
+        {
+            AutoWalkClient.AUTO_RUN_ENABLED = false;
+        }
+    }
+
+    @Inject(method = "continueDestroyBlock", at = @At("RETURN"))
+    private void onMineBlock(final BlockPos p_105284_, final Direction p_105285_, final CallbackInfoReturnable<Boolean> cir)
+    {
+        if (AutoWalk.config.getCommonConfig().stopOnInteractWithBlock && destroyTicks > 6)
+        {
+            AutoWalkClient.AUTO_RUN_ENABLED = false;
+        }
+    }
+
+    @Inject(method = "destroyBlock", at = @At("RETURN"))
+    private void onDestroyBlock(final BlockPos p_105268_, final CallbackInfoReturnable<Boolean> cir)
+    {
+        if (AutoWalk.config.getCommonConfig().stopOnInteractWithBlock && destroyTicks > 3 && cir.getReturnValue())
         {
             AutoWalkClient.AUTO_RUN_ENABLED = false;
         }
