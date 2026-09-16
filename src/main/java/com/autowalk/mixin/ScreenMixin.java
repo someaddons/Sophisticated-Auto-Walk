645e7e3c4cb72ee856786b3499a94eb10ca235fe
someaddons <someaddons@github.com>
2026-09-16 16:39:54 +0200

    26.2/3

--- src/main/java/com/autowalk/mixin/ScreenMixin.java
+++ src/main/java/com/autowalk/mixin/ScreenMixin.java
@@ -2,14 +2,14 @@ package com.autowalk.mixin;
 
 import com.autowalk.AutoWalk;
 import com.autowalk.AutoWalkClient;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(Gui.class)
 public class ScreenMixin
 {
     @Inject(method = "setScreen", at = @At("HEAD"))
